8a918cb7d6bfdd2299cf62756ba30bd9b100ff5f
Lary <110429721+Laryk96@users.noreply.github.com>
2023-01-25 04:41:14 +0200

    fix modal window

--- src/js/modal-window.js
+++ src/js/modal-window.js
@@ -1,6 +1,6 @@
 import { refs } from './refs';
 import { getTagsById } from './getTags';
-import { renderCards, createCard } from './renderCards';
+import { renderCards, createCard, renderContentForModal } from './renderCards';
 
 const KEY_STORAGE_FILMS = 'Popular movies';
 const KEY_TO_WATHED = 'Wathed list';
@@ -28,7 +28,7 @@ function openModal(event) {
   const film = getFilmOfStorageById(data, targetEl);
 
   localStorage.setItem(KEY_CURRENT_ID, JSON.stringify(film.id));
-  updateModal(film);
+  renderContentForModal(film);
   refs.modalContainer.addEventListener('click', createWatchedList);
   closeModal();
 }
@@ -64,68 +64,6 @@ function getFilmOfStorageById(films, id) {
   }
 }
 
-function updateModal(film) {
-  const {
-    original_title,
-    poster_path,
-    tags,
-    popularity,
-    vote_average,
-    overview,
-    id,
-    genre_ids,
-    vote_count,
-    title,
-  } = film;
-
-  const genre = getTagsById(genre_ids);
-  const { watched, queue, backgroundColor } = updateBtn(id);
-
-  const img = `<img src="https://image.tmdb.org/t/p/w500${poster_path}"
-   class="contents__img"
-          alt=""
-          width="280"
-          loading="lazy"/>`;
-
-  const markup = ` <h2 class="modal__title">${title}</h2>
-        <div class="modal__description">
-          <ul class="modal__list">
-            <li class="modal__item">Vote / Votes</li>
-            <li class="modal__item">Popularity</li>
-            <li class="modal__item">Original Title</li>
-            <li class="modal__item">Genre</li>
-          </ul>
-          <ul class="modal__list-value">
-            <li class="modal__item-value">
-              <span class="modal__item-value--rating vote_average">${vote_average}</span> / ${vote_count}
-            </li>
-            <li class="modal__item-value popularity">${popularity}</li>
-            <li class="modal__item-value original_title">${original_title}</li>
-            <li class="modal__item-value tags">${genre}</li>
-          </ul>
-        </div>
-        <h3 class="modal__subtitle">about</h3>
-        <p class="modal__text overview">${overview}</p>
-        <ul class="modal__btn-list">
-          <li>
-            <button
-              class="modal__btn modal__btn--to-watched "
-              data-list="watched" >
-             ${watched}
-            </button>
-          </li>
-          <li>
-            <button class="modal__btn modal__btn--to-queue ${backgroundColor}" data-list="queue">${queue}</button>
-          </li>
-        </ul>`;
-
-  refs.leftBoxModal.innerHTML = '';
-  refs.rightBoxModal.innerHTML = '';
-
-  refs.leftBoxModal.innerHTML = img;
-  refs.rightBoxModal.innerHTML = markup;
-}
-
 function createWatchedList(event) {
   const watchedBtnEl = document.querySelector('.modal__btn--to-watched');
   const queueBtnEl = document.querySelector('.modal__btn--to-queue');
@@ -142,6 +80,7 @@ function createWatchedList(event) {
     case 'watched': {
       if (wathedFilmId !== -1) {
         watchedBtnEl.textContent = 'ADD TO WATCHED';
+
         watchedFilms.splice(wathedFilmId, 1);
         localStorage.setItem(KEY_TO_WATHED, JSON.stringify(watchedFilms));
         break;
@@ -149,11 +88,16 @@ function createWatchedList(event) {
 
       if (!queueFilmId) {
         queueBtnEl.textContent = 'ADD TO WATCHED';
+        queueBtnEl.style.backgroundColor = '#fff';
+        queueBtnEl.style.color = '#000';
+        queueBtnEl.style.border = '1px solid #000';
+
         queueFilms.splice(queueFilmId, 1);
         localStorage.setItem(KEY_FOR_QUEUE, JSON.stringify(queueFilms));
       }
 
       watchedBtnEl.textContent = 'REMOVE';
+
       watchedFilms.push(targetFilm);
       localStorage.setItem(KEY_TO_WATHED, JSON.stringify(watchedFilms));
       break;
@@ -170,6 +114,7 @@ function createWatchedList(event) {
 
       if (!wathedFilmId) {
         watchedBtnEl.textContent = 'ADD TO QUEUE';
+
         watchedFilms.splice(wathedFilmId, 1);
         localStorage.setItem(KEY_TO_WATHED, JSON.stringify(watchedFilms));
       }
@@ -184,19 +129,4 @@ function createWatchedList(event) {
       break;
     }
   }
-}
-
-function updateBtn(id) {
-  const wathedFilm = JSON.parse(localStorage.getItem(KEY_TO_WATHED));
-  const queueFilm = JSON.parse(localStorage.getItem(KEY_FOR_QUEUE));
-
-  const isWathced = wathedFilm ? wathedFilm.find(film => film.id === id) : null;
-
-  const isQueue = queueFilm ? queueFilm.find(film => film.id === id) : null;
-
-  return {
-    watched: isWathced ? 'REMOVE' : 'ADD TO WATCHED',
-    queue: isQueue ? 'REMOVE' : 'ADD TO QUEUE',
-    backgroundColor: isQueue ? 'add' : '',
-  };
 }
--- src/js/renderCards.js
+++ src/js/renderCards.js
@@ -1,6 +1,9 @@
 import { refs } from './refs';
 import { getTagsById, checkTags } from './getTags';
 
+const KEY_TO_WATHED = 'Wathed list';
+const KEY_FOR_QUEUE = 'Queue list';
+
 function createCard(film) {
   const { overview, title, poster_path, release_date, genre_ids, id } = film;
 
@@ -24,10 +27,87 @@ function createCard(film) {
               </a></li>`;
 }
 
+function updateBtn(id) {
+  const wathedFilm = JSON.parse(localStorage.getItem(KEY_TO_WATHED));
+  const queueFilm = JSON.parse(localStorage.getItem(KEY_FOR_QUEUE));
+
+  const isWathced = wathedFilm ? wathedFilm.find(film => film.id === id) : null;
+
+  const isQueue = queueFilm ? queueFilm.find(film => film.id === id) : null;
+
+  return {
+    watched: isWathced ? 'REMOVE' : 'ADD TO WATCHED',
+    queue: isQueue ? 'REMOVE' : 'ADD TO QUEUE',
+    backgroundColor: isQueue ? 'add' : '',
+  };
+}
+
 function renderCards(apiData) {
   const markup = apiData.map(createCard).join('');
 
   refs.contentsList.innerHTML = markup;
 }
 
-export { renderCards, createCard };
+function renderContentForModal(film) {
+  const {
+    original_title,
+    poster_path,
+    tags,
+    popularity,
+    vote_average,
+    overview,
+    id,
+    genre_ids,
+    vote_count,
+    title,
+  } = film;
+
+  const genre = getTagsById(genre_ids);
+  const { watched, queue, backgroundColor } = updateBtn(id);
+
+  const img = `<img src="https://image.tmdb.org/t/p/w500${poster_path}"
+   class="contents__img"
+          alt=""
+          width="280"
+          loading="lazy"/>`;
+
+  const markup = ` <h2 class="modal__title">${title}</h2>
+        <div class="modal__description">
+          <ul class="modal__list">
+            <li class="modal__item">Vote / Votes</li>
+            <li class="modal__item">Popularity</li>
+            <li class="modal__item">Original Title</li>
+            <li class="modal__item">Genre</li>
+          </ul>
+          <ul class="modal__list-value">
+            <li class="modal__item-value">
+              <span class="modal__item-value--rating vote_average">${vote_average}</span> / ${vote_count}
+            </li>
+            <li class="modal__item-value popularity">${popularity}</li>
+            <li class="modal__item-value original_title">${original_title}</li>
+            <li class="modal__item-value tags">${genre}</li>
+          </ul>
+        </div>
+        <h3 class="modal__subtitle">about</h3>
+        <p class="modal__text overview">${overview}</p>
+        <ul class="modal__btn-list">
+          <li>
+            <button
+              class="modal__btn modal__btn--to-watched "
+              data-list="watched" >
+             ${watched}
+            </button>
+          </li>
+          <li>
+            <button class="modal__btn modal__btn--to-queue ${backgroundColor}" data-list="queue">${queue}</button>
+          </li>
+        </ul>`;
+
+  refs.leftBoxModal.innerHTML = '';
+  refs.rightBoxModal.innerHTML = '';
+
+  refs.leftBoxModal.innerHTML = img;
+  refs.rightBoxModal.innerHTML = markup;
+}
+
+export { renderCards, createCard, renderContentForModal };
